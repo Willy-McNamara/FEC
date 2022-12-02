88a484721fb795bcda9782f4e4563772a14d1e9f
Crynowecer0 <crynowecer11@gmail.com>
2022-12-02 10:49:05 -0500

    Created individual folders and added app.jsx component

---
+++ src/app.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  // const [ , ] = useState( );
+
+  // useEffect(() => {
+
+  // }, [])
+
+  return (
+    <div>
+      <div>Overview</div>
+      <div>Related Items</div>
+      <div>Questions and Answers</div>
+      <div>Ratings and Reviews</div>
+    </div>
+  )
+}
+
+export default App;
