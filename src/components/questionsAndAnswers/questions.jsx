e579e4f446f4be3022afa1c3920eab67f3fac5f3
Sam Noden <nodensam@gmail.com>
2022-12-02 11:34:01 -0500

    capitalized file names

---
+++ src/components/questionsAndAnswers/questions.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+
+const Questions = () => {
+  // const [ , ] = useState( );
+
+  // useEffect(() => {
+
+  // }, [])
+
+  return (
+    <div>
+      <h3>Questions and Answers</h3>
+    </div>
+  )
+}
+
+export default Questions;
