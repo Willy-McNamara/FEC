3c40ce1eecd6044756484c4c2b426782374816f8
Willy McNamara <wmcnamara11@gmail.com>
2022-12-03 19:48:46 -0600

    Redoing Commit, somethign went wrong

--- src/components/overview/components/Container1Aa.jsx
+++ src/components/overview/components/Container1Aa.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import IG1 from './IG1.jsx';
+import IG1 from './IG1/IG1.jsx';
 
 const Container1Aa = ({style, zoomHandler, zoom, indexHandler, currentIndex}) => {
 
--- src/components/overview/components/Container1Ab.jsx
+++ src/components/overview/components/Container1Ab.jsx
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import PI1 from './PI1/PI1.jsx';
+import SS1 from './SS1/SS1.jsx';
+import ATC1 from './ATC1/ATC1.jsx';
 
 const Container1Ab = ({product, style, styleHandler}) => {
 
   return (
-    <div id="Container1Aa">
+    <div id="Container1Ab">
       Container1Ab
+      < PI1 />
+      < SS1 />
+      < ATC1 />
     </div>
   )
 }
