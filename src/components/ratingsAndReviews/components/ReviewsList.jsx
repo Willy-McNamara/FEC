ad480390d73ca6a81faca0004712a7fa37ce8ff6
Crynowecer0 <crynowecer11@gmail.com>
2022-12-04 11:51:59 -0500

    rendered ReviewsSummary & ReviewsList components via main Reviews.jsx component

---
+++ src/components/ratingsAndReviews/components/ReviewsList.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+
+const ReviewsList = () => {
+  // const [ , ] = useState( );
+
+  // useEffect(() => {
+
+  // }, [])
+
+  return (
+    <div>
+      <h3>Reviews List</h3>
+    </div>
+  )
+}
+
+export default ReviewsList;
---
+++ src/components/ratingsAndReviews/components/ReviewsSummary.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+
+const ReviewsSummary = () => {
+  // const [ , ] = useState( );
+
+  // useEffect(() => {
+
+  // }, [])
+
+  return (
+    <div>
+      <h3>Reviews Summary</h3>
+    </div>
+  )
+}
+
+export default ReviewsSummary;
