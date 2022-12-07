5f5a1608dd2d4a538ff18d9ac62ab51074a0a375
Sam Noden <nodensam@gmail.com>
2022-12-07 13:40:50 -0500

    using the ol' mcnamara useeffect method

--- src/components/questionsAndAnswers/Questions.jsx
+++ src/components/questionsAndAnswers/Questions.jsx
@@ -6,22 +6,24 @@ import axios from 'axios';
 //adding comment just for fun
 
 const Questions = ({product}) => {
-  const [questions, setQuestions] = useState('');
+  const [questions, setQuestions] = useState('init');
 
   useEffect(() => {
+    if (styles === 'init') {
     axios.get(`http://localhost:3001/qa/questions/${product.id}`)
       .then((res)=>{
         setQuestions(res.data);
-        console.log('res.data:', res.data);
+        console.log('questions:', questions);
       })
       .catch((err)=>{console.log('questions get error:', err)})
-  }, []);
+    }
+  }, [questions]);
 
   return (
     <div className='questions-and-answers'>
       <h3>Questions & Answers</h3>
       <input type="text" placeholder="Search Questions" />
-      <QuestionsList produt={product}/>
+      <QuestionsList product={product}/>
       <button>Add Question</button>
     </div>
   )
