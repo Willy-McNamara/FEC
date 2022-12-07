64fde4b57fa56cefc270416c0647e0d10cdafcdd
Crynowecer0 <crynowecer11@gmail.com>
2022-12-07 12:50:08 -0500

    fixed minor typo bug in star ratings within the review tiles - they should now be rendering correctly

--- src/components/ratingsAndReviews/components/ReviewTileStarRating.jsx
+++ src/components/ratingsAndReviews/components/ReviewTileStarRating.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
-const ReviewTileStarRating = (rating) => {
-  // console.log('RATING', rating)
+const ReviewTileStarRating = ({rating}) => {
 
   return (
     <div className="ratings">
     <div className="empty-stars"></div>
-    <div className="full-stars" style={{width: `${rating}%`}}></div>
+    <div className="full-stars" style={{ width: `${rating}%`}}></div>
   </div>
   )
 }
