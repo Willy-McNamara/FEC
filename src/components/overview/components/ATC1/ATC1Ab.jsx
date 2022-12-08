9b64b361d0691e26c63a482c2a22ecd1bcda9265
Willy McNamara <wmcnamara11@gmail.com>
2022-12-08 11:45:33 -0600

    Refactored React in ATC!

--- src/components/overview/components/ATC1/ATC1Ab.jsx
+++ src/components/overview/components/ATC1/ATC1Ab.jsx
@@ -3,79 +3,83 @@ import axios from 'axios';
 import ATC1Ab1 from './ATC1Ab1.jsx';
 import ATC1Ab2 from './ATC1Ab2.jsx';
 
-const ATC1Ab= ({quants}) => {
-  //console.log('ATC1Ab render triggered')
-
-  if (quants === 'init') {
+const ATC1Ab= ({currentSize, cartHandler}) => {
+  // conditional render for first load.
+  if (currentSize[0] === 'Size') {
     return
   }
-
-  // style obj > sku property, obj of objs (SKUs as keys) > sku value obj contains key value pairs {quantity: 8, size: 'XS'}
-  let [viewQuants, setViewQuants] = useState(true)
-  let [currentQty, setCurrentQty] = useState('Select Qty')
+  let [currentQty, setCurrentQty] = useState(['Select Quantity'])
   let [qtyElements, setQtyElements] = useState('init')
-  let [oldQty, setOldQty] = useState('init')
-  let quantArray = [];
+  let [viewQuants, setViewQuants] = useState(false)
+  // mapping funciton for all available quants up to 15
 
-  for (let i = 0; i < quants; i++) {
+  // HANDLERs
+  let currentQtyHandler = (qty, size, sku) => {
+    setCurrentQty([qty, size, sku])
+  }
+  let quantViewHandler = () => {
+    setViewQuants(!viewQuants)
+  }
+
+  // Effects
+  useEffect(() => {
+    // format quants to mapable array
+    let quantArray = formatQuantArray(currentSize[1])
+    // MAP elements
+    Promise.all(quantArray.map((qty) => {
+      return (
+        < ATC1Ab2 qty={qty} size={currentSize[0]} sku={currentSize[2]} currentQtyHandler={currentQtyHandler} cartHandler={cartHandler}/>
+      )
+    }))
+    .then((elements) => {
+      setQtyElements(elements)
+    })
+    .catch((err) => {
+      console.log('error in promiseChain within ATC1Ab :', err)
+    })
+  }, [currentSize])
+
+  useEffect(() => {
+    setViewQuants(false)
+  }, [currentQty])
+
+  useEffect(() => {
+    setCurrentQty(['Select Quantity'])
+  }, [qtyElements])
+
+  // RETURN
+  if (!viewQuants) {
+    return (
+      <div id="ATC1Ab">
+        <ATC1Ab1 qty={currentQty[0]} quantViewHandler={quantViewHandler}/>
+      </div>
+    )
+  } else {
+    return (
+      <div id="ATC1Ab">
+        <ATC1Ab1 qty={'Select Quantity'} quantViewHandler={quantViewHandler}/>
+        {qtyElements}
+      </div>
+    )
+  }
+}
+
+// HELPER
+
+let formatQuantArray = (quant) => {
+  let quantArray = [];
+  let elements = 15;
+  if (quant < 15) {
+    elements = quant
+  }
+  for (let i = 0; i < elements; i++) {
     if (i === 15) {
       break;
     } else {
       quantArray.push(i+1)
     }
   }
-  // mapping funciton for all available quants up to 15
-
-  useEffect(() => {
-    //
-    Promise.all(quantArray.map((qty) => {
-      return (
-        < ATC1Ab2 qty={qty} quantViewHandler={quantViewHandler}/>
-      )
-    }))
-    .then((elements) => {
-      //
-      setQtyElements(elements)
-      setOldQty(currentQty)
-    })
-    .catch((err) => {
-      console.log('error in promiseChain within ATC1Ab :', err)
-    })
-  }, [quants, currentQty])
-
-  // HANDLER
-  let currentQtyHandler = (qty) => {
-    setCurrentQty(qty)
-  }
-
-  // HANDLER
-  let quantViewHandler = (qty) => {
-    // toggles viewSizes true/false
-    if (currentQty === qty) {
-      setViewQuants(!viewQuants)
-    } else {
-      currentQtyHandler(qty)
-    }
-  }
-
-  useEffect(() => {
-    //
-    setViewQuants(!viewQuants)
-  }, [currentQty])
-
-  if (!viewQuants) {
-    return (
-      <div id="ATC1Ab">
-        <ATC1Ab1 qty={currentQty} quantViewHandler={quantViewHandler}/>
-      </div>
-    )
-  } else {
-    return (
-      <div id="ATC1Ab">
-        {qtyElements}
-      </div>
-    )
-  }
+  return quantArray
 }
 
 export default ATC1Ab
--- src/components/overview/components/ATC1/ATC1Ab1.jsx
+++ src/components/overview/components/ATC1/ATC1Ab1.jsx
@@ -3,13 +3,10 @@ import axios from 'axios';
 
 const ATC1Ab1= ({qty, quantViewHandler}) => {
 
-  // style obj > sku property, obj of objs (SKUs as keys) > sku value obj contains key value pairs {quantity: 8, size: 'XS'}
-
   return (
     <div id="ATC1Ab1" onClick={(e) => {
       e.preventDefault();
-      // console.log('size onClick triggered. here is size :', qty)
-      quantViewHandler(qty)
+      quantViewHandler()
     }}>
       {qty}
     </div>
