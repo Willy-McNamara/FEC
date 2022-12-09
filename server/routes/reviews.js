42d80cb8da98566ed387c87bda65dd8b2c52db02
Crynowecer0 <crynowecer11@gmail.com>
2022-12-09 08:04:26 -0500

    implemented controllers server side, confirmed reviewsSummary.jsx is receiving correct object back

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -44,13 +44,22 @@ axios.get(apiURL + `/reviews/?product_id=${req.params.product_id}`, {
 
 //this is VERY similar to the above request, but purpose is to get meta data. refactor this eventually after functionality is hit.
 router.get('/meta/:product_id', (req, res)=>{
-  console.log('/meta/product_id', req.params)
+  // FIXME:console.log('/meta/product_id', req.params)
   axios.get(apiURL + `/reviews/meta/?product_id=${req.params.product_id}`, {
     headers:
      {'Authorization': APIKEY}}
     )
     .then((data)=>{
-      res.status(200).send(data.data)
+      console.log('DATA FROM /meta/:product_ID', data.data)
+      console.log('Testing calcRating Average Function in Ratings Object', calcRatingAverages(data.data.ratings) )
+      console.log('Testing totalRatings function', countRatings(data.data.ratings) )
+      const sendToClient = {
+        countOfRatings: countRatings(data.data.ratings),
+        ratingAverages: calcRatingAverages(data.data.ratings),
+        rawData: data.data
+      }
+      console.log('testing send to client', sendToClient)
+      res.status(200).send(sendToClient)
       logInteraction({
         'element': 'Reviews.jsx',
         'widget': 'reviews',
@@ -87,5 +96,20 @@ let calcRatingAverages = (ratingsObject) => {
     ratingAsPercentRounded: ratingAsPercentRounded
   }
 }
+
+//count total # of ratings
+const countRatings = (ratingsObject) => {
+  // console.log('RATINGS OBJECT', ratingsObject)
+  let countOfRatings = 0;
+
+  for (let key in ratingsObject) {
+    countOfRatings += +ratingsObject[key]
+  }
+
+  return countOfRatings
+}
+
+
+
 
 module.exports = router
