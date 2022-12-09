f59020be8c35333ba19c0a1233226bc636353ac2
Crynowecer0 <crynowecer11@gmail.com>
2022-12-09 14:19:49 -0500

    sucessfully rendered first image - no examples currently have more than 1

--- src/components/ratingsAndReviews/components/ReviewTile.jsx
+++ src/components/ratingsAndReviews/components/ReviewTile.jsx
@@ -1,32 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import ReviewTileStarRating from './ReviewTileStarRating.jsx'
+import ReviewPhoto from './ReviewPhoto.jsx'
 
-const ReviewTile = (
-  { review:
-    {
-      review_id,
-      rating,
-      reviewer_name,
-      summary,
-      body,
-      helpfulness,
-      response,
-      date,
-      photos,
-    }
-  }) => {
+const ReviewTile = ({ review }) => {
+  const { review_id, rating, reviewer_name, summary, body, helpfulness, response, date, photos } = review
+
+  console.log('Photos in rev tile', photos);
 
   return (
     <div className="border">
-      <div className = 'flex flex-row space-between'>
-        <ReviewTileStarRating rating={rating * 20}/>
+      <div className='flex flex-row space-between'>
+        <ReviewTileStarRating rating={rating * 20} />
         <span>{reviewer_name && <span>{reviewer_name}, </span>} {new Date(date).toDateString()}</span>
       </div>
       <h4>{summary}</h4>
       <div>{body}</div>
       {response && <div>{response}</div>}
       {/* TODO: build out the picture component at a basic level */}
-      {photos.length > 0 &&<div>{photos.length}</div>}
+      {photos.length > 0 ? <div>{photos.map((photo, index)=>{
+        return (<ReviewPhoto key={index} photo={photo} />)
+      })}</div> : console.log('no photo')}
       {/* TODO: build out the helpful button clicking functionality */}
       <span>Helpful?</span>
       <span>{helpfulness}</span>
