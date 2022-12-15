af3fcb8c371254c9912f490d81350a2fecc3ad08
Crynowecer0 <crynowecer11@gmail.com>
2022-12-14 21:16:03 -0500

    review text expands and only the first 250 chars show up by default

--- src/components/ratingsAndReviews/components/ReviewTile.jsx
+++ src/components/ratingsAndReviews/components/ReviewTile.jsx
@@ -4,10 +4,29 @@ import PhotoModal from './PhotoModal.jsx'
 
 const ReviewTile = ({ review, score }) => {
   const { review_id, rating, reviewer_name, summary, body, helpfulness, response, date, photos } = review
-  //FIXME: why did this cause an infinite re-render cycle??
+
   const [showModal, setShowModal] = useState(false);
+  const [isExpanded, setExpanded] = useState(false);
 
+  const countChars = (str, max) => {
+    let result = str
+    if (str.length >= max) {
+      result = result.slice(0, max)
+    }
+    if (str.length > max) {
+      result += '...'
+    }
+    return result
+  }
 
+  const buttonText = () => {
+    return isExpanded ? 'Show less' : 'Show more'
+  }
+
+  const handleBodyToggle = (e) => {
+    e.preventDefault()
+    setExpanded(!isExpanded)
+  }
 
   // console.log('raw review from review tile', review)
   // console.log('score from reviewtile', helpfulness)
@@ -18,9 +37,16 @@ const ReviewTile = ({ review, score }) => {
         <ReviewTileStarRating rating={rating * 20} />
         <span>{reviewer_name && <span>{reviewer_name}, </span>} {new Date(date).toDateString()}</span>
       </div>
-      <h4>{summary}</h4>
-      <div>{body}</div>
+      <h4>{countChars(summary, 60)}</h4>
+      <div>{!isExpanded ? countChars(body, 250) : body}</div>
+      {review.body.length > 250 && (
+          <a href="#" onClick={handleBodyToggle}>
+            {buttonText()}
+          </a>
+        )}
+
       {response && <div>{response}</div>}
+      <div>
       {photos.length > 0 ?
         <div className="border">{photos.map((photo, index) => {
           //FIXME: will need to refactor
@@ -34,11 +60,12 @@ const ReviewTile = ({ review, score }) => {
             </div>
           )
         })}</div> : undefined}
+        </div>
       {/* TODO: build out the helpful button clicking functionality */}
       <span>Helpful?</span>
       <span>{helpfulness}</span>
       {/* TODO: build */}
-      <span>Report button</span>
+      <p>Report button</p>
     </div>
   )
 }
